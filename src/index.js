81b992aef384c52d64237b63a39cd8cda68b6854
unclegreg <grzesiekwitczak@gmail.com>
2017-09-05 23:03:54 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import css from './index.styl';
 import $ from 'jquery';
-import 'fullpage.js';
-
-$(document).ready(function() {
-   $('#fullpage').fullpage({
-     anchors:['home', 'about', 'services', 'contact'],
-   });
-});
 
 (function () {
   $('.nav-menu').on('click', function() {
