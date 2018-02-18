2bbac28bd801db6ef212a561477d179e5c8b5628
Grzegorz Witczak <grzegorz.witczak@ansta.pl>
2018-02-18 09:35:19 +0100

    version 1.0.0

--- README.md
+++
@@ -1,1 +0,0 @@
-Felskowski.com website
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import css from './index.styl';
-import $ from 'jquery';
-
-(function () {
-  $('.nav-menu').on('click', function() {
-    $('.bar').toggleClass('animate');
-    $('.page-menu').toggleClass('menu-opened');
-  })
-})();
